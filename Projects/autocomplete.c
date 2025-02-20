0b07b0f015d63f5e7acfd525e2e2c62b1a7c57a2
jakkii <licc06ca@gmail.com>
2025-02-19 22:46:16 -0500

    Co-authored-by: Laaaarry <Laaaarry@users.noreply.github.com>

--- Projects/autocomplete.c
+++ Projects/autocomplete.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include "autocomplete.h"
 
+// compare function for qsort
+int compare (const void *a, const void *b) {
+    return strcmp(((struct term *)a)->term, ((struct term *)b)->term);
+}
+
 void read_in_terms(struct term **terms, int *pnterms, char *filename) {
     // opens file, and checks if file is NULL
     FILE *fp = fopen(filename, "r");
@@ -16,16 +21,48 @@ void read_in_terms(struct term **terms, int *pnterms, char *filename) {
     *terms = (struct term *) malloc(sizeof(struct term) * *pnterms);
     // gets the term and weight from each term
     for (int i = 0; i < *pnterms; i++) {
-        fscanf(fp, "%ld\t%s", &(*terms)[i].weight, (*terms)[i].term);
+        fscanf(fp, "%lf\t%[^\n]", &(*terms)[i].weight, (*terms)[i].term);
     }
+    qsort(*terms, *pnterms, sizeof(struct term), compare);
+    fclose(fp);
 }
 
 int lowest_match(struct term *terms, int nterms, char *substr) {
-
+    int result = -1;
+    int low = 0;
+    int high = nterms - 1;
+    int mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (strcmp(terms[mid].term, substr, strlen(substr)) < 0) {
+            low = mid + 1;
+        } else if (strcmp(terms[mid].term, substr, strlen(substr)) > 0){
+            high = mid - 1;
+        } else {
+            result = mid;
+            high = mid - 1;
+        }    
+    }
+    return result;
 }
 
 int highest_match(struct term *terms, int nterms, char *substr) {
-
+    int result = -1;
+    int low = 0;
+    int high = nterms - 1;
+    int mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (strcmp(terms[mid].term, substr, strlen(substr)) > 0) {
+            high = mid - 1;
+        } else if (strcmp(terms[mid].term, strlen(substr)) < 0) {
+            low = mid + 1;
+        } else{
+            result = mid;
+            low = mid + 1;
+        }
+    }
+    return result;
 }
 
 void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr) {
