f3c8f9ae1d6a02d299c1322fd9651b25731c63b0
jakkii <licc06ca@gmail.com>
2025-04-13 02:17:33 -0400

    Initial Exam review

---
+++ exam/DFS&BFS.py
@@ -0,0 +1,79 @@
+# Transversing Graphs
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.connections = []
+        self.visited = False
+
+def connect(node1, node2):
+    node1.connections.append(node2)
+    node2.connections.append(node1)
+
+A = Node("A")
+B = Node("B")
+C = Node("C")
+D = Node("D")
+E = Node("E")
+F = Node("F")
+
+connect (A, B)
+connect (E, F)
+connect (B, E)
+connect (E, D)
+connect (B, D)
+connect (B, C)
+connect (D, C)
+
+# BFS (Breadth First Search)
+def BFS(node):
+    q = [node]
+    node.visited = True
+    while len(q) > 0:
+        cur = q.pop(0)
+        print(cur.name)
+        for con in cur.connections:
+            if not con.visited:
+                q.append(con)
+                con.visited = True
+
+BFS(C)
+
+A.visited = False
+B.visited = False
+C.visited = False
+D.visited = False
+E.visited = False
+F.visited = False
+
+print()
+
+# DFS (Depth First Search)
+def DFS(node):
+    q = [node]
+    node.visited = True
+    while len(q) > 0:
+        cur = q.pop()
+        print (cur.name)
+        for con in cur.connections:
+            if not con.visited:
+                q.append(con)
+                con.visited = True
+DFS(C)
+
+A.visited = False
+B.visited = False
+C.visited = False
+D.visited = False
+E.visited = False
+F.visited = False
+
+print()
+
+def DFS_rec(node):
+    print(node.name)
+    node.visited = True
+    for con in node.connections:
+        if not con.visited:
+            DFS_rec(con)
+DFS_rec(C)
+
---
+++ exam/Dijkstra.py
@@ -0,0 +1,65 @@
+# Shortest Path Algorithm
+
+# Weighted Graph
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.connections = []
+        self.visited = False
+
+def connect (node1, node2, weight):
+    node1.connections.append({"node": node2, "weight": weight})
+    node2.connections.append({"node": node1, "weight": weight})
+
+A = Node("A")
+B = Node("B")
+C = Node("C")
+D = Node("D")
+E = Node("E")
+F = Node("F")
+
+connect(A, B, 3)
+connect(E, F, 8)
+connect(B, E, 7)
+connect(E, D, 5)
+connect(B, D, 1)
+connect(B, C, 2)
+connect(D, C, 4)
+
+def get_all_nodes(node):
+    connections = []
+    q = [node]
+    node.visited = True
+    while len(q) > 0:
+        cur = q.pop(0)
+        connections.append(cur)
+        for con in cur.connections:
+            if not con["node"].visited:
+                q.append(con["node"])
+                con["node"].visited = True
+    return connections
+
+def dijsktra(node):
+
+    S = [node]
+    d = {node.name:0}
+    prev = {}
+
+    unexplored = get_all_nodes(node)
+    
+    for n in unexplored:
+        if n.name not in d:
+            d[n.name] = 999999
+
+    while len(unexplored) > 0:
+        cur = unexplored.pop(0)
+        for con in cur.connections:
+            if con["node"] not in S:
+                if con["weight"] + d[cur.name] < d[con["node"].name]:
+                    d[con["node"].name] = con["weight"] + d[cur.name]
+
+        S.append(cur)
+
+    return d
+
+print(dijsktra(C))
